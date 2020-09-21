551c2374db98139b5805a5c44357147c916aa560
Alex Keil <alexpkeil1@users.noreply.github.com>
2020-09-21 16:26:08 -0400

    sacrifice examples at the altar of CRAN

--- Rpkg/metropolis/R/conversions.R
+++ Rpkg/metropolis/R/conversions.R
@@ -13,12 +13,14 @@ as.mcmc.metropolis.samples <- function(x, ...){
   #' @export
   #' @method as.mcmc metropolis.samples
   #' @examples
+  #' \dontrun{
   #' library("coda")
   #' dat = data.frame(y = rbinom(100, 1, 0.5), x1=runif(100), x2 = runif(100))
   #' res = metropolis_glm(y ~ x1 + x2, data=dat, family=binomial(), iter=10000, burnin=3000, 
   #' adapt=TRUE, guided=TRUE, block=FALSE)
   #' res2 = as.mcmc(res)
   #' summary(res2)
+  #' }
   samples = x$parms[x$parms$burn==0,grep('logsigma|b_', names(x$parms), value = TRUE, fixed=FALSE)]
   mcmc(data= samples, 
        start =  x$burnin, 
